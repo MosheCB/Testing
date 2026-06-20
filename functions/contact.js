9e4384402bad81ba8ea643bed2970a779595e4b7
MosheCB <moshebrownstein@proton.me>
2026-06-20 01:08:57 -0400

    Update email recipient in contact.js

--- functions/contact.js
+++ functions/contact.js
@@ -4,7 +4,7 @@ const resend = new Resend('re_TbyLdKhR_A1XgishPy2VDwnQW365wNGKt');
 
 resend.emails.send({
   from: 'onboarding@resend.dev',
-  to: 'moshebrownstein@proton.me',
+  to: 'moshebrownsteinlcsw@gmail.com',
   subject: 'Hello World',
   html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
 });
